7217826cdeb8035327e3e27422a11d2764f6ddb7
hamzathul dilshad <56413189+hdaamzz@users.noreply.github.com>
2025-12-08 12:30:29 +0530

    Refactor Queue implementation to use array

--- queue/queue.js
+++ queue/queue.js
@@ -1,17 +1,15 @@
 class Queue {
   constructor() {
-    this.items = {};
+    this.items = [];
     this.front = 0;
     this.rear = 0;
   }
 
-  // Add item to end
   enqueue(value) {
     this.items[this.rear] = value;
     this.rear++;
   }
 
-  // Remove and return first item
   dequeue() {
     if (this.isEmpty()) return null;
 
@@ -21,38 +19,28 @@ class Queue {
     return value;
   }
 
-  // Check first element without removing
   peek() {
     return this.isEmpty() ? null : this.items[this.front];
   }
 
-  // Number of items in queue
   size() {
     return this.rear - this.front;
   }
 
-  // True if no items
   isEmpty() {
     return this.size() === 0;
   }
 
-  // Remove all elements
   clear() {
-    this.items = {};
+    this.items = [];
     this.front = 0;
     this.rear = 0;
   }
 
-  // Shows clean array version of queue
   toArray() {
-    const arr = [];
-    for (let i = this.front; i < this.rear; i++) {
-      arr.push(this.items[i]);
-    }
-    return arr;
+    return this.items.slice(this.front, this.rear);
   }
 
-  // Debug print
   print() {
     console.log(this.toArray());
   }
